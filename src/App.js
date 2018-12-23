2736ccc801bb8748b0f99eab6866281a0a7c3a95
MRKMOD <Bigrift@gmail.com>
2018-12-23 15:11:18 +0100

    updated files to move to Github

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
+function Main() {
+  return (
+    <div className="main">
+      <h1>React and Webpack with Webpack server on watch mode</h1>
+      <p>
+        This is a very old project and it should be updated to the latest
+        modules.
+      </p>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
 
-ReactDOM.render(
-<Contacts />,
-    document.getElementById('app')
-);
+ReactDOM.render(<Main />, document.getElementById("app"));
